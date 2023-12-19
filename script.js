aa4644b388ddad8c83cf22a22e82737e906994c1
Khang Duong Van <dvkhang2100321@student.ctuet.edu.vn>
2023-12-19 19:52:50 +0700

    sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector(".menu-icon");
+const sidebar = document.querySelector(".sidebar");
+menuIcon.addEventListener("click", function () {
+  sidebar.classList.toggle("small-sidebar");
+});
